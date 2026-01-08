a02f6f7d64f2786b74e86cc4658dfea3f820b81c
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2026-01-08 15:55:14 +0200

    Added the Reviews block

--- js/main.js
+++ js/main.js
@@ -26,3 +26,38 @@ platformSlider.on('resize', function () {
 });
 
 platformSlider.mount();
+
+
+const reviewSlider = new Splide( '.js-review-slider', {
+    type   : 'loop',
+    padding: '300px',
+    pagination: false,
+} );
+
+reviewSlider.on('resize', function () {
+    let p = '300px';
+    if ($(window).width() < 1200) {
+        p = '150px';
+    }
+    if ($(window).width() < 991) {
+        p = '100px';
+    }
+    if ($(window).width() < 767) {
+        p = '0px';
+    }
+    reviewSlider.options = {
+        padding: p,
+    };
+    reviewSlider.refresh()
+});
+
+function updateSlideCounter() {
+    const currentSlideElm = document.getElementById('review-current-slide');
+    const totalSlidesElm = document.getElementById('review-total-slides');
+    currentSlideElm.textContent = reviewSlider.index + 1;
+    totalSlidesElm.textContent = reviewSlider.length;
+}
+
+reviewSlider.on('move', updateSlideCounter);
+reviewSlider.mount();
+updateSlideCounter();
