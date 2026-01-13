3fae0565f62dcb87bcbcf1317c59c1236c3081f5
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2026-01-13 10:46:28 +0200

    Implemented the mobile main menu

--- tripium/resources/views/components/header.blade.php
+++ tripium/resources/views/components/header.blade.php
@@ -3,17 +3,20 @@
     <div class="fixed">
         <div class="d-flex align-items-center justify-content-between header-height">
             <div><a href="#"><img src="/img/tripium-logo.svg" alt="Tripium"/></a></div>
-            <div>
-                <ul class="header-menu">
+            <div class="header-menu js-header-menu">
+                <ul>
                     <li><a href="#products">Products</a></li>
                     <li><a href="#solutions">Solutions</a></li>
                     <li><a href="#about-us">About Us</a></li>
                     <li><a href="#">FAQ</a></li>
                     <li><a href="#">Publications</a></li>
+                    <li><a href="#contact-us" class="d-md-none d-block">Contact Us</a></li>
                 </ul>
+                <a class="cursor-pointer menu-close js-menu-close"><img src="/icon/x.svg" alt="" /></a>
             </div>
             <div>
-                <a href="#contact-us" class="btn btn-outline-secondary fw-semibold">Contact Us</a>
+                <a href="#contact-us" class="btn btn-outline-secondary fw-semibold d-md-inline-block d-none">Contact Us</a>
+                <a class="cursor-pointer menu-open js-menu-open"><img src="/icon/menu.svg" alt="" /></a>
             </div>
         </div>
     </div>
