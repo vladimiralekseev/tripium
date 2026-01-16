8c1a2e8d2b5179de606ae638bd349159fe87458c
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2026-01-16 10:29:51 +0200

    Add 404 page layout

---
+++ tripium/resources/views/components/head.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Tripium</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link type="image/png" rel="icon" href="/favicon.ico">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon:wght@400&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <link href="/css/splide.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/general.css?v=2.0" rel="stylesheet" type="text/css"/>
+
+    {!! RecaptchaV3::initJs() !!}
+</head>
---
+++ tripium/resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="fixed h-100 d-flex align-content-center flex-column justify-content-center">
+        <h1 class="text-center">404<br />Page not found</h1>
+    </div>
+@endsection
